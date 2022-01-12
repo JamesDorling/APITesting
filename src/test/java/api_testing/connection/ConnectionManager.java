3b410671715a6e560bde3d450c5ddb84b9fc1e3e
ultamatewizard <james.lance.smith@hotmail.co.uk>
2022-01-12 17:02:52 +0000

    Added ConnectionManager

---
+++ src/test/java/api_testing/connection/ConnectionManager.java
@@ -0,0 +1,64 @@
+package api_testing.connection;
+import api_testing.Config.Config;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConnectionManager {
+
+    private static final String BASEURL = "https://openweathermap.org/";
+    private static final String ENDPOINT = "data/2.5/weather";
+
+    public static String getConnection() {
+        getResponse();
+        return getURL();
+    }
+
+    public static String getConnection(String cityName) {
+        return getURL() + "?q=" + cityName + "&appid=" + Config.getApiKey();
+    }
+
+    public static String getConnection(String cityName, String stateCode) {
+        return getURL() + "?q=" + cityName + "," + stateCode + "&appid=" + Config.getApiKey();
+    }
+
+    public static String getConnection(String cityName, String stateCode, String countryCode) {
+        return getURL() + "?q=" + cityName + "," + stateCode + "," + countryCode + "&appid=" + Config.getApiKey();
+    }
+
+    public static String getConnection(int cityId) {
+        return getURL() + "?id=" + cityId + "&appid=" + Config.getApiKey();
+    }
+
+    public static String getConnection(double lat, double lon) {
+        return getURL() + "?lat=" + lat + "&lon=" + lon + "&appid=" + Config.getApiKey();
+    }
+
+    public static String getConnection(int zipCode, String countryCode) {
+        return getURL() + "?zip=" + zipCode + "," + countryCode + "&appid=" + Config.getApiKey();
+    }
+
+    public static int getStatusCode() {
+        return getResponse().statusCode();
+    }
+
+    private static HttpResponse<String> getResponse() {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(BASEURL + ENDPOINT)).build();
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return httpResponse;
+    }
+
+    public static String getURL() {
+        return BASEURL + ENDPOINT;
+    }
+
+}
